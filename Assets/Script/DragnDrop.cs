cebc36ae16fb598eb6210e3b29ad8d06ea560023
ax350 <ax350@nyu.edu>
2021-04-09 21:45:23 -0400

    Initial Backup

---
+++ Assets/Script/DragnDrop.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragnDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField]
+    private Canvas canvas;
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    [SerializeField]
+    private GameObject parentPanel;
+
+    public void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        parentPanel = this.transform.parent.gameObject;
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        Cursor.visible = false;
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = .5f;
+
+        if (parentPanel.name == "Frame")
+        {
+            parentPanel.GetComponent<ItemSlot>().activeSelf(true);
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrag");
+        //Debug.Log(eventData.delta / canvas.scaleFactor + " " + eventData.delta + " " + canvas.scaleFactor);
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        Cursor.visible = true;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+    }
+
+    //public void OnDrop(PointerEventData eventData)
+    //{
+    //    throw new System.NotImplementedException();
+    //}
+
+    public void changeParent(Transform input)
+    {
+        transform.SetParent(input);
+        parentPanel = input.gameObject;
+    }
+}
---
+++ Assets/Script/ItemSlot.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    private RectTransform rectTransform;
+    //private Image image;
+    [SerializeField]
+    private GameObject Hint;
+    public void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        //image = GetComponent<Image>();
+    }
+    void IDropHandler.OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        var currentObject = eventData.pointerDrag;
+        var object_rectTransform = currentObject.GetComponent<RectTransform>();
+        if (currentObject != null)
+        {
+            //currentObject.transform.SetParent(transform);
+            currentObject.GetComponent<DragnDrop>().changeParent(transform);
+            object_rectTransform.anchoredPosition = rectTransform.anchoredPosition + new Vector2(rectTransform.sizeDelta.x/2, -rectTransform.sizeDelta.y/2);
+        }
+
+        //activeSelf(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void activeSelf(bool cmd)
+    {
+        //image.SetEnabled(cmd);
+        Hint.SetActive(cmd);
+    }
+}
